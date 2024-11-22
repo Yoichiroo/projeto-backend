a4d9147cb593a217f4819ae7f11fee854e0238a5
Lucas Torres <109477475+Yoichiroo@users.noreply.github.com>
2024-11-22 13:41:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,31 @@ import express from "express";
 
 const publicacoes = [
     {
+        id: 1,
         desc: "Um gatinho fofo dormindo",
         img: "https://placecats.com/cute/300/200",
 
     },
     {
+        id: 2,
         desc: "Gato brincando com um novelo de lã",
         img: "https://placecats.com/playful/400/300",
 
     },
     {
+        id: 3,
         desc: "Olhar penetrante de um gato preto",
         img: "https://placecats.com/black/500/250",
 
     },
     {
+        id: 4,
         desc: "Gato explorando a natureza",
         img: "https://placecats.com/outdoor/600/400",
 
     },
     {
+        id: 5,
         desc: "Gatinho ronronando no colo",
         img: "https://placecats.com/cuddle/300/200",
     }
@@ -37,5 +42,16 @@ app.listen(3000, ()=>{
 
 app.get("/publicacoes", (req, res) => {
     res.status(200).json(publicacoes);
+});
+
+function buscarPost(id) {
+    return publicacoes.findIndex((publicacao)=> {
+        return publicacao.id === Number(id);
+    })
+};
+
+app.get("/publicacoes/:id", (req, res) => {
+    const index = buscarPost(req.params.id);
+    res.status(200).json(publicacoes[index]);
 });
 
