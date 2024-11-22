559464aa10198a771006abce4aba6485cbb15d82
Lucas Torres <109477475+Yoichiroo@users.noreply.github.com>
2024-11-22 12:57:06 -0300

    API de teste criada

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 import express from "express";
 
+const publicacoes = [
+    {
+        desc: "Um gatinho fofo dormindo",
+        img: "https://placecats.com/cute/300/200",
+
+    },
+    {
+        desc: "Gato brincando com um novelo de lã",
+        img: "https://placecats.com/playful/400/300",
+
+    },
+    {
+        desc: "Olhar penetrante de um gato preto",
+        img: "https://placecats.com/black/500/250",
+
+    },
+    {
+        desc: "Gato explorando a natureza",
+        img: "https://placecats.com/outdoor/600/400",
+
+    },
+    {
+        desc: "Gatinho ronronando no colo",
+        img: "https://placecats.com/cuddle/300/200",
+    }
+];
+
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, ()=>{
     console.log("ta on o server paizao");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("faaaala mulekos e molekas!!!");
+app.get("/publicacoes", (req, res) => {
+    res.status(200).json(publicacoes);
 });
 
