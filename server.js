2047e131e69033373300c840f3ca1e7d45d6b704
Lucas Torres <109477475+Yoichiroo@users.noreply.github.com>
2024-11-23 18:53:08 -0300

    Criado a conexão com banco de dados

--- server.js
+++ server.js
@@ -1,57 +1,24 @@
 import express from "express";
+import dbConnect from "./src/config/configBd.js";
 
-const publicacoes = [
-    {
-        id: 1,
-        desc: "Um gatinho fofo dormindo",
-        img: "https://placecats.com/cute/300/200",
-
-    },
-    {
-        id: 2,
-        desc: "Gato brincando com um novelo de lã",
-        img: "https://placecats.com/playful/400/300",
-
-    },
-    {
-        id: 3,
-        desc: "Olhar penetrante de um gato preto",
-        img: "https://placecats.com/black/500/250",
-
-    },
-    {
-        id: 4,
-        desc: "Gato explorando a natureza",
-        img: "https://placecats.com/outdoor/600/400",
-
-    },
-    {
-        id: 5,
-        desc: "Gatinho ronronando no colo",
-        img: "https://placecats.com/cuddle/300/200",
-    }
-];
+const conexao = await dbConnect(process.env.CONEXAO);
 
 const app = express();
 
 app.use(express.json());
 
 app.listen(3000, ()=>{
-    console.log("ta on o server paizao");
+    console.log("Servidor ligado");
 });
 
-app.get("/publicacoes", (req, res) => {
-    res.status(200).json(publicacoes);
+async function getTodasPublicacoes() {
+    const database =  conexao.db("pentagram");
+    const colecao = database.collection("publicacoes");
+
+    return colecao.find().toArray();
+}
+
+app.get("/publicacoes", async (req, res) => {
+    const resultadoPublicacoes = await getTodasPublicacoes();
+    res.status(200).json(resultadoPublicacoes);
 });
-
-function buscarPost(id) {
-    return publicacoes.findIndex((publicacao)=> {
-        return publicacao.id === Number(id);
-    })
-};
-
-app.get("/publicacoes/:id", (req, res) => {
-    const index = buscarPost(req.params.id);
-    res.status(200).json(publicacoes[index]);
-});
-
