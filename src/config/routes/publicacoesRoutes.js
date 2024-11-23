bea188c635aa696daf613baa533823c0d87fbe9e
Lucas Torres <109477475+Yoichiroo@users.noreply.github.com>
2024-11-23 19:49:58 -0300

    Update publicacoesRoutes.js

--- src/config/routes/publicacoesRoutes.js
+++ src/config/routes/publicacoesRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { listarPublicacoes } from "../controllers/publicacoesController.js";
+
 const routes = (app) => {
     app.use(express.json());
-    app.get("/publicacoes", listarPublicacoes());
+    app.get("/publicacoes", listarPublicacoes);
 };
 
 export default routes;
